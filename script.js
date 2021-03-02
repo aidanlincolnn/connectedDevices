082fa81d9f53fe1fee32ddfddbe869a9fe56f468
Aidan Fowler <fowler.aidan@gmail.com>
2021-03-01 21:24:27 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,53 @@
 /*
   Generates QR code for current url to pass to phone. Uses 
-  https://github.com/kazuhikoarase/qrcode-generator
-  as the QR Code generator library. It's hosted at this CDN:
-  https://unpkg.com/qrcode-generator@1.4.4/qrcode.js
-  created 11 Feb 2021
-  Aidan Fowler
-  boilerplate code from Tom Igoe
-*/
+  
+  
+  // The serviceUuid must match the serviceUuid of the device you would like to connect
+// Copyright (c) 2018 p5ble
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT*/
 
-function setQrCode() {
-    // get the div element for the QR code image:
-    let qrDiv = document.getElementById('qrCode');
-    // set qr code to current url
-    let url = window.location.href;
-    // make the QR code:
-    let qr = qrcode(0, 'L');
-    console.log('url:'+url);
-    qr.addData(url);
-    qr.make();
-    // create an image from it:
-    let qrImg = qr.createImgTag(4, 8, "qr code of " + url);
-    // add it to the div:
-    qrDiv.innerHTML = qrImg;
+const serviceUuid = "19B10000-E8F2-537E-4F6C-D104768A1214";
+let myCharacteristic;
+let input;
+let myBLE;
+
+function setup() {
+  myBLE = new p5ble();
+}
+
+function connectToBle() {
+  // Connect to a device by passing the service UUID
+  myBLE.connect(serviceUuid, gotCharacteristics);
+}
+
+function test(){
+  document.getElementById("connected").removeAttribute("hidden");
+  document.getElementById("p1").innerHTML = "Status: Connected!";
+  document.getElementById("connectButton").style.display="none";
+}
+
+function gotCharacteristics(error, characteristics) {
+  if (error) {
+    console.log('error: ', error);
   }
+  else{
+    document.getElementById("connected").removeAttribute("hidden");
+    document.getElementById("p1").innerHTML = "Status: Connected!";
+    document.getElementById("p1").style.color="rgb(148, 255, 110)";
+    document.getElementById("connectButton").style.display="none";
+  }
+  console.log('characteristics: ', characteristics);
+  // Set the first characteristic as myCharacteristic
+  myCharacteristic = characteristics[0];
+}
 
-  // add a listener for the page to load:
-window.addEventListener('DOMContentLoaded', setQrCode);
+function writeToBle() {
+  var messageVal = document.getElementById("message").value;
+  // Write the value of the input to the myCharacteristic
+  myBLE.write(myCharacteristic, messageVal);
+}
+
+// add a listener for the page to load:
+window.addEventListener('DOMContentLoaded', setup);
