46053fd234831be53588e4bbfdfea982fb502168
Aidan Fowler <fowler.aidan@gmail.com>
2021-04-13 13:41:54 -0400

    Update s3.env.js

--- rpiNodeGifBackup/nodeServer/app/config/s3.env.js
+++ rpiNodeGifBackup/nodeServer/app/config/s3.env.js
@@ -1,6 +1,6 @@
 const env = {
-	AWS_ACCESS_KEY: 'AKIA42AONS6RCIYZHEVB', // change to yours
-	AWS_SECRET_ACCESS_KEY: 'cPPQf1I83g3rtF7ojHZMOJxKn9I/wwdmiD4waQyy', // change to yours
+	AWS_ACCESS_KEY: 'xxxx', // change to yours
+	AWS_SECRET_ACCESS_KEY: 'xxx/xxx', // change to yours
 	REGION : 'us-east-1', // change to yours
 	Bucket: 'infinitecontent' // change to yours
 };
